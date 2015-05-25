ac49775009ccf16f6d8a2f6c47538d272a50f16d
haachicanoy <harold22010@gmail.com>
2015-05-24 22:20:08 -0500

    Few additions

--- fitting_models.R
+++ fitting_models.R
@@ -13,8 +13,11 @@ data$tramo <- factor(data$tramo)
 data$periodo <- factor(data$periodo)
 str(data)
 
-pairs(data[,-c(1:2)])
-cor(data[,-c(1:2)],use='complete.obs')
+# Pearson correlation matrix
+write.csv(round(cor(data[,-c(1:2)],use='complete.obs',method='pearson'),2),'correlation_matrix_pearson.csv',row.names=T)
+# Spearman correlation matrix
+write.csv(round(cor(data[,-c(1:2)],use='complete.obs',method='spearman'),2),'correlation_matrix_spearman.csv',row.names=T)
+
 
 ## Variables de respuesta: morfologicas
 # 1. Pendiente del cauce
@@ -39,9 +42,65 @@ cor(data[,-c(1:2)],use='complete.obs')
 # 3. Coeficiente de uniformidad
 # 4. Carga media anual de material del lecho
 
-lm.fit <- lm()
+# Correlaciones entre variables por decada
+cor.by.period <- lapply(as.numeric(levels(data$periodo)),function(i)
+{
+  data.by.period <- data %>% filter(periodo==i)
+  data.by.period <- data.by.period[,-c(1:2)]
+  cor.mat <- cor(data.by.period,method='spearman',use='pairwise.complete.obs')
+  cor.mat <- as.data.frame(cor.mat)
+  cor.mat <- round(cor.mat,2)
+  cor.mat$varName <- rownames(cor.mat)
+  rownames(cor.mat) <- 1:nrow(cor.mat)
+  cor.mat$periodo <- i
+  return(cor.mat)
+})
+cor.by.period <- Reduce(function(...) rbind(..., deparse.level=1), cor.by.period)
+rownames(cor.by.period) <- 1:nrow(cor.by.period)
+write.csv(cor.by.period,'correlations_by_period.csv',row.names=FALSE)
+
+# Correlaciones entre variables por tramo
+cor.by.tramo <- lapply(as.numeric(levels(data$tramo)),function(i)
+{
+  data.by.tramo <- data %>% filter(periodo==i)
+  data.by.tramo <- data.by.tramo[,-c(1:2)]
+  cor.mat <- cor(data.by.tramo,method='spearman',use='pairwise.complete.obs')
+  cor.mat <- as.data.frame(cor.mat)
+  cor.mat <- round(cor.mat,2)
+  cor.mat$varName <- rownames(cor.mat)
+  rownames(cor.mat) <- 1:nrow(cor.mat)
+  cor.mat$tramo <- i
+  return(cor.mat)
+})
+cor.by.tramo <- Reduce(function(...) rbind(..., deparse.level=1), cor.by.tramo)
+rownames(cor.by.tramo) <- 1:nrow(cor.by.tramo)
+write.csv(cor.by.tramo,'correlations_by_tramo.csv',row.names=FALSE)
+
+# Fit linear regression models by response variable
+resList <- names(data)[3:9]
+lm.fit.all <- lapply(1:length(resList),function(i)
+{
+  label <- paste('lm.fit <- lm(',resList[i],'~ tramo + periodo + caudal_medio + caudal_maximo + caudal_50 + caudal_banca + velocidad + ancho_superficial + diametro_promedio + dev_granulometrica + coef_uniformidad + carga_media, data = data)')
+  eval(parse(text=label)); rm(label)
+  return(lm.fit)
+})
+
+# Fit linear regression models by response variable including 
+lm.fit.all.time <- lapply(1:length(resList),function(i)
+{
+  library(nlme)
+  label <- paste('lm.fit <- gls(',resList[i],'~ tramo + periodo + caudal_medio + caudal_maximo + caudal_50 + caudal_banca + velocidad + ancho_superficial + diametro_promedio + dev_granulometrica + coef_uniformidad + carga_media, data = data, na.action = na.omit, correlation = corAR1(form = ~periodo|tramo))')
+  eval(parse(text=label)); rm(label)
+  return(lm.fit)
+})
+
 
 library(nlme)
+
+gls()
+
+fit.lmm <- lme(t_movilidad_max ~ tramo + caudal_medio + caudal_maximo + caudal_50 + caudal_banca + velocidad + ancho_superficial + diametro_promedio + dev_granulometrica + coef_uniformidad + carga_media,
+               random = ~1 | periodo, data = data)
 
 # My first mixed model
 # Using lme4 package
