1531e2c2e62d58092b643ded6bc3c727210fea4d
haachicanoy <harold22010@gmail.com>
2016-06-10 18:32:41 -0500

    Today work

--- _occupational_therapy/_scripts/inclusion_laboral.R
+++ _occupational_therapy/_scripts/inclusion_laboral.R
@@ -4,7 +4,9 @@
 
 library(readxl)
 inDir <- 'C:/Users/haachicanoy/Documents/GitHub/Statistical_consulting/_occupational_therapy/_data/_modified_version'
-all_data <- read_excel(paste(inDir, '/all_data.xlsx', sep=''), sheet=1)
+all_data <- read_excel(paste(inDir, '/all_data_final_coded.xlsx', sep=''), sheet=1)
+
+# ANALISIS DESCRIPTIVO
 
 library(ggplot2)
 # Boxplot Sexo vs Edad
@@ -30,8 +32,8 @@ box()
 dev.off()
 
 # Grafico de barras educacion vs condicion de discapacidad
-all_data$Educación_formal <- factor(all_data$Educacion_formal, levels=c('Ninguno','Primaria','Bachillerato','Técnico','Tecnología','Profesional'), ordered=TRUE)
-gg <- ggplot(all_data, aes(Educación_formal, fill=Grupo)) + geom_bar() + theme_bw()
+all_data$Educacion_formal <- factor(all_data$Educacion_formal, levels=c('Ninguno','Primaria','Bachillerato','Técnico','Tecnología','Profesional'), ordered=TRUE)
+gg <- ggplot(all_data, aes(Educacion_formal, fill=Grupo)) + geom_bar() + theme_bw()
 gg <- gg + theme(axis.text.x = element_text(size=14),
                  axis.text.y = element_text(size=14),
                  axis.title.x = element_text(face="bold",size=15),
@@ -44,7 +46,98 @@ gg <- gg + scale_fill_discrete(name="Grupo estudiado",
 gg <- gg + xlab('Grado más alto de educación formal alcanzado') + ylab('Número de individuos')
 ggsave(filename='C:/Users/haachicanoy/Documents/GitHub/Statistical_consulting/_occupational_therapy/_results/barplot_eduGrupo.pdf', plot=gg, width=9, height=7, units='in')
 
-ggplot(all_data, aes(Educación_formal)) + geom_bar() + facet_wrap(~ Grupo) + theme_bw()
+ggplot(all_data, aes(Educacion_formal)) + geom_bar() + facet_wrap(~ Grupo) + theme_bw()
+
+
+# ANALISIS DE CORRELACION
+
+all_data$Ingreso_hogar <- factor(all_data$Ingreso_hogar, levels=c('Menos de un salario mínimo','Un salario mínimo','Entre 2 y 3 salarios mínimos','Entre 4 y 5 salarios mínimos','Más de 5 salarios mínimos'), ordered=TRUE)
+all_data$Nivel_discapacidad <- factor(all_data$Nivel_discapacidad, levels=c('Ninguno','Leve','Moderado','Severo','Profundo'), ordered=TRUE)
+
+### Factores ambientales
+mtch_fa <- grep(pattern='^FA', colnames(all_data))
+for(i in 1:length(mtch_fa)){
+  all_data[,mtch_fa[i]] <- factor(all_data[,mtch_fa[i]], levels=c('No aplica',
+                                                                  'No hay barrera','Barrera ligera','Barrera moderada','Barrera grave','Barrera completa',
+                                                                  'No hay facilitador','Facilitador ligero','Facilitador moderado','Facilitador grave','Facilitador completo'), ordered=TRUE)
+}; rm(i, mtch_fa)
+
+### Apoyo y servicios
+mtch_ap <- grep(pattern='^APS', colnames(all_data))
+for(i in 1:length(mtch_ap)){
+  all_data[,mtch_ap[i]] <- factor(all_data[,mtch_ap[i]], levels=c('No','Si, ninguno','Si, alguno pero no suficiente','Si, suficiente'), ordered=TRUE)
+}; rm(i, mtch_ap)
+
+### Calidad de vida
+mtch_cv <- grep(pattern='^CV', colnames(all_data))
+odd_numbers <- unlist(lapply(1:length(mtch_cv), function(i){
+  sst <- strsplit(colnames(all_data)[mtch_cv][i], "")[[1]]
+  out <- paste0(sst[c(TRUE, FALSE)], sst[c(FALSE, TRUE)])
+  out <- as.numeric(out[3])
+  is.even <- function(x) x %% 2 == 0
+  out <- is.even(out)
+  return(out)
+}))
+
+# Importancia
+mtch_cv_imp <- mtch_cv[!odd_numbers]
+for(i in 1:length(mtch_cv_imp)){
+  all_data[,mtch_cv_imp[i]] <- factor(all_data[,mtch_cv_imp[i]], levels=c('Poco importante','Algo importante','Medianamente importante','Muy importante','Crucialmente importante'), ordered=TRUE)
+}; rm(i, mtch_cv_imp)
+
+mtch_cv_sat <- mtch_cv[odd_numbers]
+# Satisfaccion
+for(i in 1:length(mtch_cv_sat)){
+  all_data[,mtch_cv_sat[i]] <- factor(all_data[,mtch_cv_sat[i]], levels=c('Muy insatisfecho','Insatisfecho','Neutral','Satisfecho','Muy satisfecho'), ordered=TRUE)
+}; rm(i, mtch_cv_sat)
+
+# str(all_data)
+
+# Test Chi-cuadrado de independencia
+
+# Chi-square test for original data
+options(warn=-1)
+p.chisq = matrix(0, nrow=ncol(all_data), ncol=ncol(all_data), byrow=T)
+for(i in 1:ncol(all_data)){
+  for(j in 1:ncol(all_data)){
+    p.chisq[i,j] = round(chisq.test(all_data[,i],all_data[,j])$p.value,3)
+  }
+}; rm(i); rm(j)
+
+diag(p.chisq) = NA
+colnames(p.chisq) = colnames(all_data)
+rownames(p.chisq) = colnames(all_data)
+
+library(gplots)
+color_scale = colorRampPalette(c("tomato3","lightyellow","lightseagreen"), space="rgb")(50)
+pdf('C:/Users/haachicanoy/Documents/GitHub/Statistical_consulting/_occupational_therapy/_results/chi_test.pdf', height=7, width=7)
+plot.new()
+heatmap.2(p.chisq,
+          main="Independence test",
+          key.title="Chi-square test",
+          key.xlab="p-value",
+          Rowv=NULL,
+          Colv=NULL,
+          col=color_scale,
+          linecol=NULL,
+          tracecol=NULL,
+          density.info="density",
+          denscol="blue",
+          margins=c(5,5))
+dev.off()
+
+sum(as.numeric(p.chisq[upper.tri(p.chisq)])<0.05)/length(as.numeric(p.chisq[upper.tri(p.chisq)]))
+
+write.csv(p.chisq,"C:/Users/haachicanoy/Documents/GitHub/Statistical_consulting/_occupational_therapy/_results/chisq_matrix.csv", row.names=T)
+rm(p.chisq, color_scale)
+
+
+# CUANTIFICACION OPTIMA
+
+res <- homals(galo, active = c(rep(TRUE, 4), FALSE), sets = list(c(1,3,4),2,5))
+res$low.rank # Quantified variables
+
+
 
 all_data <- all_data[complete.cases(all_data),]
 
