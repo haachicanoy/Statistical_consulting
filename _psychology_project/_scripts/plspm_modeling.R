4fe4f2bc368f9ef61c1824dd9b930be9ce6f6ca4
Harold Achicanoy <haachicanoy@Harolds-MacBook-Pro.local>
2019-02-19 23:38:05 -0500

    Labeling the script

--- _psychology_project/_scripts/plspm_modeling.R
+++ _psychology_project/_scripts/plspm_modeling.R
@@ -1,29 +1,42 @@
-in_data %>% dplyr::select(Q_10:Q_18_relaciones)     # People
-in_data %>% dplyr::select(Q_19:Q_31_no_permanece)   # Knowledge
-in_data %>% dplyr::select(Q_32:Q_62_rutinaria)      # Organization
-in_data %>% dplyr::select(Q_63)                     # Management
-in_data %>% dplyr::select(Q_65:Q_69_redes_sociales) # Knowledge management
-in_data %>% dplyr::select(Q_70:Q_80)                # Technology
-in_data %>% dplyr::select(Q_81:Q_84)                # Innovation
+# Practicas innovadoras - Proyecto Univalle Psicologia: modelamiento de rutas
+# H. Achicanoy
+# 2019
 
-indx <- sapply(in_data, is.factor)
-in_data2 <- in_data
+# R options
+options(warn = -1, scipen = 999)
+
+# Load packages
+suppressMessages(library(plspm))
+
+## Just to know the structure
+# in_data %>% dplyr::select(Q_10:Q_18_relaciones)     # People
+# in_data %>% dplyr::select(Q_19:Q_31_no_permanece)   # Knowledge
+# in_data %>% dplyr::select(Q_32:Q_62_rutinaria)      # Organization
+# in_data %>% dplyr::select(Q_63)                     # Management
+# in_data %>% dplyr::select(Q_65:Q_69_redes_sociales) # Knowledge management
+# in_data %>% dplyr::select(Q_70:Q_80)                # Technology
+# in_data %>% dplyr::select(Q_81:Q_84)                # Innovation
+
+## Transforming factor to numeric for all variables of interest
+indx           <- sapply(in_data, is.factor)
+in_data2       <- in_data
 in_data2[indx] <- lapply(in_data2[indx], function(x) as.numeric(x))
 
-Personas     <- rep(0, 7)
-Conocimiento <- rep(0, 7)
-Organizacion <- rep(0, 7)
-Gestion      <- rep(0, 7)
-Tecnologia   <- rep(0, 7)
+## Defining the relationships in the model
+Personas             <- rep(0, 7)
+Conocimiento         <- rep(0, 7)
+Organizacion         <- rep(0, 7)
+Gestion              <- rep(0, 7)
+Tecnologia           <- rep(0, 7)
 Gestion_conocimiento <- c(1, 1, 1, 1, 1, 0, 0)
 Innovacion           <- c(0, 0, 0, 0, 0, 1, 0)
-inv_path     <- rbind(Personas, Conocimiento, Organizacion, Gestion, Tecnologia, Gestion_conocimiento, Innovacion)
-# add optional column names
-colnames(inv_path) <- rownames(inv_path)
+inv_path             <- rbind(Personas, Conocimiento, Organizacion, Gestion, Tecnologia, Gestion_conocimiento, Innovacion)
+colnames(inv_path)   <- rownames(inv_path)
+
+## Looking the structural model
 plspm::innerplot(inv_path)
 
-inv_modes  <- c(rep("B", 7))
-
+## Defining blocks
 inv_blocks  <- list(8:17,    # Personas
                     18:47,   # Conocimiento
                     48:85,   # Organizacion
@@ -32,6 +45,7 @@ inv_blocks  <- list(8:17,    # Personas
                     87:104,  # Gestion del conocimiento
                     115:117) # Innovacion
 
+## Defining the type of scaling parameter for each variable
 inv_scaling <- list(c(rep("NOM", 2), rep("ORD", 4), rep("NOM", 4)), # Personas
                     c(rep("ORD", 9), rep("NOM", 21)), # Conocimiento
                     c(rep("NOM", 7), rep("ORD", 13), rep("NOM", 18)), # Organizacion
@@ -41,23 +55,42 @@ inv_scaling <- list(c(rep("NOM", 2), rep("ORD", 4), rep("NOM", 4)), # Personas
                     c("NOM", rep("ORD", 2)) # Innovacion
                     )
 
-# PLS-PM
-inv_pls <- plspm::plspm(in_data2, inv_path, inv_blocks, scaling = inv_scaling,
-                 modes = inv_modes, scheme = "path", plscomp = rep(1, 7), tol = 0.0000001)
+## Running the models
+# Formative mode
+inv_pls1 <- plspm::plspm(in_data2, inv_path, inv_blocks, scaling = inv_scaling,
+                         modes = rep("B", 7), scheme = "path", plscomp = rep(1, 7), tol = 0.0000001)
 
+# Reflective mode
 inv_pls2 <- plspm::plspm(in_data2, inv_path, inv_blocks, scaling = inv_scaling,
-                 modes = c(rep("A", 7)), scheme = "path", plscomp = rep(1, 7), tol = 0.0000001)
+                         modes = c(rep("A", 7)), scheme = "path", plscomp = rep(1, 7), tol = 0.0000001)
 
+# newA mode
 inv_pls3 <- plspm::plspm(in_data2, inv_path, inv_blocks, scaling = inv_scaling,
                          modes = c(rep("newA", 7)), scheme = "path", plscomp = rep(1, 7), tol = 0.0000001)
 
+# PLScore mode
 inv_pls4 <- plspm::plspm(in_data2, inv_path, inv_blocks, scaling = inv_scaling,
                          modes = c(rep("PLScore", 7)), scheme = "path", plscomp = rep(1, 7), tol = 0.0000001)
 
+# PLScow mode
 inv_pls5 <- plspm::plspm(in_data2, inv_path, inv_blocks, scaling = inv_scaling,
                          modes = c(rep("PLScow", 7)), scheme = "path", plscomp = rep(1, 7), tol = 0.0000001)
 
-# Jackknife
+## Plotting results from model
+plot_res <- function(plsmod = inv_pls2){
+  
+  df <- plsmod$scores %>% base::as.data.frame()
+  df$Class <- in_data$Clasificacion
+  
+  gg <- df %>% ggplot2::ggplot(aes(x = Gestion_conocimiento, y = Innovacion, colour = Class)) +
+    geom_point()
+  
+  return(print(gg))
+}
+plot_res(inv_pls2)
+cor(inv_pls2$scores, method = "pearson") %>% corrplot.mixed(., lower = "number", upper = "square", tl.cex )
+
+## Jackknifing the model. Removing one company at the time
 models <- lapply(1:nrow(in_data2), function(i){
   tryCatch(expr={
     fit <- plspm::plspm(in_data2[-i,], inv_path, inv_blocks, scaling = inv_scaling,
@@ -104,42 +137,3 @@ centroids = ddply(inv_scores,
                   AvgInnovacion = mean(Innovacion))
 
 inv_rebus = rebus.pls(inv_pls2)
-
-
-
-
-
-coefs <- NA
-for(i in 1:length(models)){
-  coefs[i] <- models[[i]]$path_coefs[3,2]
-}
-
-plot(inv_pls)
-
-data(russett)
-
-# path matrix (inner model realtionships)
-AGRIN = c(0, 0, 0)
-INDEV = c(0, 0, 0)
-POLINS = c(1, 1, 0)
-rus_path = rbind(AGRIN, INDEV, POLINS)
-# add optional column names
-colnames(rus_path) = rownames(rus_path)
-
-# list indicating what variables are associated with what latent variables
-rus_blocks = list(1:3, 4:5, 6:11)
-
-# all latent variables are measured in a reflective way
-rus_modes = rep("A", 3)
-
-# run plspm analysis
-rus_pls = plspm(russett, rus_path, rus_blocks, modes = rus_modes)
-
-models <- lapply(1:nrow(russett), function(i){
-  fit <- plspm(russett[-i,], rus_path, rus_blocks, modes = rus_modes)
-})
-
-coefs <- NA
-for(i in 1:length(models)){
-  coefs[i] <- models[[i]]$path_coefs[3,2]
-}
